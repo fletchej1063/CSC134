dff74081aab4a8126f845ec65f735978ac364069
fletchej1063 <46724996+fletchej1063@users.noreply.github.com>
2019-01-17 13:56:31 -0500

    Add files via upload

---
+++ M1HW_Fletcher/main.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M1HW1
+// Joseph Fletcher
+// 1/17/2019
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Provide a brief introduction
+    cout << "My name is Joseph Fletcher" << endl;
+    cout << "I am currently working on my associates in engineering" << endl;
+    cout << "I am "<< 22 << " years old." << endl;
+    cout << "My favorite books include:" << endl;
+    cout << "\tPercy Jackson" << endl;
+    cout << "\tHitchhikers Guide to the Galaxy" << endl;
+    return 0;
+}
