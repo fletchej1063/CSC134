13549b8e01df41a7582aec7e9fe65ea92dc20b4a
fletchej1063 <46724996+fletchej1063@users.noreply.github.com>
2019-05-14 14:03:45 -0400

    Original

---
+++ M7T1-2_Fletcher/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string word;
+    ifstream infile("../MyFile");
+    infile >> word;
+    cout << word << endl;
+    infile.close();
+    return 0;
+}
